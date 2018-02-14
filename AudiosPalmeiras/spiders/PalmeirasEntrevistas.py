45aaf3aa2e6a662593a14ef2bff2430f01426eea
nicolastarzia <github@nicolastarzia.com>
2018-02-13 22:35:40 -0200

    Trying to fix duplicated urls

--- AudiosPalmeiras/spiders/PalmeirasEntrevistas.py
+++ AudiosPalmeiras/spiders/PalmeirasEntrevistas.py
@@ -1,18 +1,31 @@
 # -*- coding: UTF-8 -*-
 import scrapy
 from scrapy.http import FormRequest
+import json
+import os.path
+
 class PalmeirasSpider(scrapy.Spider):
     name = "Audios"
     urlAudio = 'http://www.palmeiras.com.br/audios/ajax_audios'
     countItens = 4
     page = 1
+    dataAlreadyScraped = []
 
     def start_requests(self):
+        if os.path.isfile('audios.json'):
+            with open('audios.json') as data_files:
+                self.dataAlreadyScraped = json.load(data_files)
         frmData = {
          'page': str(self.page),
          'count': str(self.countItens)
         }
         return [FormRequest(self.urlAudio, formdata=frmData, callback=self.parse)]
+
+    def alExists(self, data, value):
+        if data is None:
+            return False
+        return len([x for x in data if x['urlFile'] == value]) > 0
+
     def parse(self, response):
         audios = response.css('.boxAudio')
         if(audios != []): #Se nao tiver mais informacoes a variavel nao é preenchida
@@ -20,6 +33,11 @@ class PalmeirasSpider(scrapy.Spider):
                 calendario = boxAudio.css('.calAudio::text').extract_first()
                 titulo = boxAudio.css('.titAudio::text').extract_first()
                 urlFile = boxAudio.css('::attr(file)').extract_first()
+                data = self.dataAlreadyScraped
+                print(self.alExists)
+                alExst = self.alExists(data=data, value=urlFile)
+                if(alExst == True):
+                    return
                 yield {
                     'calendario': calendario,
                     'titulo': titulo,
